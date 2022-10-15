0f9f802e8ce65e6dcb49105fe4e3dde9e63f2100
Robert David Ramirez Garcia <robert.ramirez.garcia@gmail.com>
2022-10-15 11:59:30 -0500

    Proyecto generado con start spring io

---
+++ src/main/java/com/bsoftgroup/springcloudmsconsulta/SpringCloudMsConsultaApplication.java
@@ -0,0 +1,13 @@
+package com.bsoftgroup.springcloudmsconsulta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudMsConsultaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudMsConsultaApplication.class, args);
+	}
+
+}
